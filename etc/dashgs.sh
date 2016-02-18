50c6f0cb597a0ab4bc6c6313148d71b3a6a21e96
Mariano Obarrio Miles <mariano.obarrio@gmail.com>
2016-02-18 13:47:53 +0100

    Update wiki y renamre de www por server.js

--- etc/dashgs.sh
+++ etc/dashgs.sh
@@ -11,24 +11,24 @@
 export NODE_PATH=$NODE_PATH:/usr/lib/node_modules:/var/adm/ssoo/dashboard/node_modules
 case "$1" in
 	start)
-		RUNNING=`ps -ef|grep "/usr/bin/node /var/adm/ssoo/dashboard/bin/www"|grep -v grep |wc -l`
+		RUNNING=`ps -ef|grep "/usr/bin/node /var/adm/ssoo/dashboard/bin/server.js"|grep -v grep |wc -l`
 		if [ "$RUNNING" -eq "1" ]; 
 		then 
 		 	echo "failed. Service is running."
 			exit 0; 
 		fi
 		echo -n "Starting dashboard..."
-		exec /usr/bin/forever start -s --minUptime 10000  --spinSleepTime 1000 /var/adm/ssoo/dashboard/bin/www 2>&1 >/dev/null
+		exec /usr/bin/forever start -s --minUptime 10000  --spinSleepTime 1000 /var/adm/ssoo/dashboard/bin/server.js 2>&1 >/dev/null
 		echo "done."
 		;;
 	stop)
 		echo -n "Stopping dashboard..."
-		exec /usr/bin/forever stop /var/adm/ssoo/dashboard/bin/www 2>&1 >/dev/null
+		exec /usr/bin/forever stop /var/adm/ssoo/dashboard/bin/server.js 2>&1 >/dev/null
 		echo "done."
 		;;
 	restart)
 		echo -n "Restarting dashboard..."
-		exec /usr/bin/forever restart /var/adm/ssoo/dashboard/bin/www 2>&1 >/dev/null
+		exec /usr/bin/forever restart /var/adm/ssoo/dashboard/bin/server.js 2>&1 >/dev/null
 		echo "done."
 		;;
 	*)
